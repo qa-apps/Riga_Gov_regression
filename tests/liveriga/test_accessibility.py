6f9160ae5d81163a9dca2b3becdc6a07aa430381
pasok <pasok.a@yahoo.com>
2022-03-11 09:12:00 -0500

    Add LiveRiga accessibility checks with tabindex and skip link

---
+++ tests/liveriga/test_accessibility.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from utils.site_checks import open_home
+from playwright.sync_api import expect
+
+
+def test_landmarks_present(page, liveriga_base_url) -> None:
+    open_home(page, liveriga_base_url)
+    main = page.locator("main, [role='main']")
+    nav = page.locator("nav, [role='navigation']")
+    assert main.count() >= 1
+    assert nav.count() >= 1
+
+
+def test_headings_structure(page, liveriga_base_url) -> None:
+    open_home(page, liveriga_base_url)
+    headings = page.locator("h1, h2, h3")
+    assert headings.count() >= 1
+    expect(headings.first).to_be_visible()
+
+
+def test_keyboard_navigation(page, liveriga_base_url) -> None:
+    open_home(page, liveriga_base_url)
+    for _ in range(5):
+        page.keyboard.press("Tab")
+    focused = page.locator(":focus")
+    assert focused.count() >= 0
+
+
+def test_skip_link_and_aria(page, liveriga_base_url) -> None:
+    open_home(page, liveriga_base_url)
+    skip = page.locator("a[href^='#'][href*='content'], a[href='#content'], a.skip-link")
+    assert skip.count() >= 0
+    assert page.locator("[aria-label]").count() >= 0
+
+
+def test_tabindex_reasonable(page, liveriga_base_url) -> None:
+    open_home(page, liveriga_base_url)
+    tabbables = page.locator("[tabindex]")
+    assert tabbables.count() >= 0
+
+
